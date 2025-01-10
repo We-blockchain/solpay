29101e401ef33d9fc1d08a55767f0c16f3d3edfe
anonymous <viktoryamineva@gmail.com>
2025-01-11 00:26:07 +0800

    Feat: custom endpoint

--- src/connection.ts
+++ src/connection.ts
@@ -3,10 +3,17 @@ import { type Cluster, clusterApiUrl, Connection } from '@solana/web3.js';
 var connection = new Connection(clusterApiUrl('mainnet-beta'));
 
 /**
+ * Example:
+ * ```
+ * setCluster('devnet');
+ * var connection = getConnection();
+ * 
+ * var connection = setCluster(null, `https://mainnet.helius-rpc.com/?api-key=${key}`);
+ * ```
  * @see {@link getConnection}
  */
-export function setCluster(cluster?: Cluster): Connection {
-    connection = new Connection(clusterApiUrl(cluster ? cluster : 'mainnet-beta'));
+export function setCluster(cluster?: Cluster | null, customEndpoint?: string): Connection {
+    connection = new Connection(customEndpoint || clusterApiUrl(cluster ? cluster : 'mainnet-beta'));
     return connection;
 }
 
